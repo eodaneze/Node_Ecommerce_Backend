062159bbbf287b2c93d7b61df9e64a01e001dcf4
Ezeali Daniel Okechukwu <ezealiokechukwu1999@gmail.com>
2022-11-07 07:02:03 -0800

    changes made in deployment

--- server.js
+++ server.js
@@ -35,11 +35,11 @@ app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRouter);
 
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
+// const __dirname = path.resolve();
+// app.use(express.static(path.join(__dirname, '/frontend/build')));
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+// );
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
