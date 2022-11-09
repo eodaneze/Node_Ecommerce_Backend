9f4d1173744f9b333d4a80c1ff8d131fff39d71b
Ezeali Daniel Okechukwu <ezealiokechukwu1999@gmail.com>
2022-11-09 03:41:12 -0800

    changing the send message in ther server

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
-import path from 'path'
+import path from "path";
 import dotenv from "dotenv";
 import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -19,21 +20,25 @@ mongoose
   });
 const app = express();
 
+let corsOptions = {
+  orgin: "*",
+  crendentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/api/keys/paypal', (req, res) => {
-  res.send(process.env.PAYAL_CLIENT_ID || 'sb');
+app.get("/api/keys/paypal", (req, res) => {
+  res.send(process.env.PAYAL_CLIENT_ID || "sb");
 });
-
 
 app.use("/api/seed", seedRouter);
 // test
 app.use("/api/products", productRouter);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRouter);
-
 
 // const __dirname = path.resolve();
 // app.use(express.static(path.join(__dirname, '/frontend/build')));
@@ -42,9 +47,9 @@ app.use("/api/orders", orderRouter);
 // );
 
 app.use((err, req, res, next) => {
+  res.send("The server is running");
   res.status(500).send({ message: err.message });
 });
-
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
